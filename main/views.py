89311e347901a08b7db9521592b7edce1b4e6355
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-09-16 18:44:00 +0700

    feat: Add view data with XML and JSON

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import ProductEntryForm
 from main.models import ProductEntry
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     products = ProductEntry.objects.all()
@@ -19,3 +21,19 @@ def create_product_entry(request):
 
     context = {'form': form}
     return render(request, "create_product_entry.html", context)
+
+def show_xml(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
