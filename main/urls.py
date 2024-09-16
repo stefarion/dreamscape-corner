f7260b54f34fac2a69ee49b1858961fe79a7fae6
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-09-16 16:51:59 +0700

    feat: Add form to register products

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_product_entry
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-product-entry', create_product_entry, name='create_product_entry'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import ProductEntry
 
 def show_main(request):
-    context = {
-        'name' : 'Firefly Doll',
-        'price': '850000',
-        'description': 'Elevate your collection with the Honkai: Star Rail Firefly Doll!',
-        'category': 'Plush'
-    }
+    products = ProductEntry.objects.all()
 
+    context = {
+        'products': products
+    }
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
