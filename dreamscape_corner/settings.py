0b06e7a259610581d7311e0c22f5e9d4f1f2b69e
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-09-16 12:08:39 +0700

    build: Add deploy.yml

--- dreamscape_corner/settings.py
+++ dreamscape_corner/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://stefanus-tan-dreamscapecorner.pbp.cs.ui.ac.id", "https://stefanus-tan-dreamscapecorner.pbp.cs.ui.ac.id"]
