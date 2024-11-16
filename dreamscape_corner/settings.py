aced6e0c8b9fb95167a8fb479ea4ff28ec3554e3
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-11-17 00:49:01 +0700

    feat: Add authentication app

--- dreamscape_corner/settings.py
+++ dreamscape_corner/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-12pw7ujkv$sn10in!bl9c!nrg4ofo*i3vre1*gf&vl&07m5$0*
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stefanus-tan-dreamscapecorner.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "stefanus-tan-dreamscapecorner.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'dreamscape_corner.urls'
@@ -73,6 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dreamscape_corner.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -50,6 +52,24 @@ def add_product_ajax(request):
     new_product.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = ProductEntry.objects.create(
+            user=request.user,
+            name=data["name"],
+            price=int(data["price"]),
+            category=data["category"],
+            description=data["description"]
+        )
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 def edit_product(request, id):
     product = ProductEntry.objects.get(pk = id)
